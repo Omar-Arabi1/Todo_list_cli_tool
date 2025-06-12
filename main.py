0813bea4c6e0a5a1a49e19a511de0e6b6af8d9ff
Omar-Arabi1 <omarbuisness7@gamil.com>
2025-06-12 06:03:29 +0300

    changed a couple of things in readme to make it accurate to how the program runs

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import typer
 
-def main(name):
+app = typer.Typer()
+
+@app.command()
+def hello(name):
     print(f"Hello {name}")
+
+
+@app.command()
+def goodbye(name, formal = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
+
+if __name__ == "__main__":
+    app()
