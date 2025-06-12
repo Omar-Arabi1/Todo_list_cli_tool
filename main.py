14d0d73f8901ccb64a50b320a5fc8b39d1a3b118
Omar-Arabi1 <omarbuisness7@gamil.com>
2025-06-12 07:07:15 +0300

    made the user have the ability to add multiple tasks at once and they get saved in a json file

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import json
 
 todo_list = typer.Typer()
 
-tasks = {
-    "do homework": False
-}
+tasks = {}
 
 @todo_list.command()
 def add_task(task: str):
-    tasks.update({task: False})
+    tasks_split = task.split(", ")
+    if len(tasks_split) == 1:
+        tasks.update({task: False})
+    else:
+        for i in tasks_split:
+            tasks.update({i: False})
+
     with open("tasks.json", mode="w", encoding="utf-8") as write_file:
-        json.dump(tasks, write_file, indent=1)
+        json.dump(tasks, write_file, indent=4)
 
 @todo_list.command()
 def list_tasks():
